4167b4717b6e8eb5e617e5c27942fb8dc501790a
Will Hutcheon <willhutcheon@gmail.com>
2024-10-08 22:17:05 -0400

    db connector

---
+++ models/db-conn.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const sqlite = require("better-sqlite3");
+const path = require("path");
+const db = new sqlite(path.join(__dirname, "../.data", "database.db"));
+
+function all(sql, ...params) {
+    return db.prepare(sql).all(params);
+}
+
+function get(sql, ...params) {
+    return db.prepare(sql).get(params);
+}
+
+function run(sql, ...params) {
+    return db.prepare(sql).run(params[0]);
+}
+
+module.exports = {
+    all,
+    get,
+    run,
+};
