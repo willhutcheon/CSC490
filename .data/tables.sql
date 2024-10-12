a54a8cc71f2ebeeeb8fa6f51ef47e1fa1495b4d3
Will <81497725+willhutcheon@users.noreply.github.com>
2024-10-12 13:35:16 -0400

    Update tables.sql

--- .data/tables.sql
+++ .data/tables.sql
@@ -9,7 +9,6 @@ CREATE TABLE IF NOT EXISTS users (
     created_at datetime NOT NULL,
     PRIMARY KEY (user_id)
 );
-
 CREATE TABLE IF NOT EXISTS workout_plans (
     plan_id int NOT NULL UNIQUE,
     user_id int NOT NULL,
@@ -20,7 +19,6 @@ CREATE TABLE IF NOT EXISTS workout_plans (
     PRIMARY KEY (plan_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 ); 
-
 CREATE TABLE IF NOT EXISTS workouts (
     workout_id int NOT NULL UNIQUE,
     plan_id int NOT NULL,
@@ -32,7 +30,6 @@ CREATE TABLE IF NOT EXISTS workouts (
     PRIMARY KEY (workout_id),
     FOREIGN KEY (plan_id) REFERENCES workout_plans(plan_id) ON DELETE CASCADE
 );  
-
 CREATE TABLE IF NOT EXISTS exercises (
     exercise_id int NOT NULL UNIQUE,
     workout_id int NOT NULL,
@@ -64,7 +61,6 @@ CREATE TABLE IF NOT EXISTS feedback (
     FOREIGN KEY (exercise_id) REFERENCES exercises(exercise_id) ON DELETE CASCADE ,
     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );  
-
 CREATE TABLE IF NOT EXISTS user_preferences (
     preference_id int NOT NULL UNIQUE,
     user_id int NOT NULL,
@@ -75,10 +71,6 @@ CREATE TABLE IF NOT EXISTS user_preferences (
     PRIMARY KEY (preference_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
-
-
-
-
 CREATE TABLE IF NOT EXISTS user_feedback (
     user_id INT,
     workout_id INT,
@@ -90,8 +82,6 @@ CREATE TABLE IF NOT EXISTS user_feedback (
 
 
 
-
-
 INSERT INTO `users` (`user_id`, `fname`, `lname`, `username`, `email`, `fit_goal`, `exp_level`, `created_at`)
 VALUES
   (7572, "Ursa", "Moses", "Joseph F. Dudley", "vulputate.dui.nec@icloud.ca", "Strength", "Advanced", "2025-01-09 01:16:10"),
@@ -99,7 +89,6 @@ VALUES
   (2710, "Lucius", "Puckett", "Benedict Y. Meadows", "cras.sed@yahoo.net", "Hypertrophy", "Intermediate", "2015-01-09 01:16:10"),
   (3601, "Connor", "Hurst", "Chaim D. Guy", "vel.arcu@aol.net", "Strength", "Advanced", "2024-01-09 01:16:10"),
   (2624, "Lila", "Henry", "Bryar Y. Richards", "egestas.urna@icloud.net", "Endurance", "Beginner", "2020-01-09 01:16:10");
-
 INSERT INTO `workout_plans` (`plan_id`, `user_id`, `plan_name`, `start_date`, `end_date`, `active`)
 VALUES
   (84179, 7572, "plan1", "2025-01-09", "2025-05-26", TRUE),
@@ -107,7 +96,6 @@ VALUES
   (80156, 2710, "plan3", "2015-01-09", "2025-07-22", TRUE),
   (23446, 3601, "plan4", "2024-01-09", "2025-07-21", FALSE),
   (47699, 2624, "plan5", "2020-01-09", "2024-09-21", FALSE);
-
 INSERT INTO `workouts` (`workout_id`, `plan_id`, `wk_date`, `cals`, `type`, `intensity`, `duration`)
 VALUES
   (48867, 84179, "2024-10-14", 772, "Strength", "Advanced", 30),
@@ -115,7 +103,6 @@ VALUES
   (94137, 80156, "2024-08-14", 353, "Hypertrophy", "Intermediate", 20),
   (83779, 23446, "2025-06-16", 147, "Strength", "Advanced", 15),
   (65927, 47699, "2025-05-05", 533, "Endurance", "Beginner", 10);
-
 INSERT INTO `workout_performance` (`perf_id`, `exercise_id`, `actual_sets`, `actual_reps`, `actual_weight`, `perf_date`)
 VALUES
   (10, 1, 3, 13, 13.5, "2019-02-09 02:16:10"),
@@ -123,7 +110,6 @@ VALUES
   (30, 3, 9, 2, 11.5, "2024-03-14 04:18:10"),
   (40, 4, 1, 10, 3.00, "2023-04-19 05:40:10"),
   (50, 5, 4, 13, 11.5, "2015-01-05 06:55:10");
-
 INSERT INTO `feedback` (`feedback_id`, `exercise_id`, `user_id`, `rating`)
 VALUES
   (100, 1, 7572, 10),
@@ -131,29 +117,23 @@ VALUES
   (300, 3, 2710, 8),
   (400, 4, 3601, 7),
   (500, 5, 2624, 7);
-
 INSERT INTO `exercises` (`exercise_id`, `workout_id`, `api_id`, `plan_sets`, `plan_reps`, `plan_weight`, `rest_time`, `exercise_name`)
 VALUES
   (6, 48867, 1024, 4, 12, 50, 60, "Chest Press"),
   (7, 39681, 1025, 3, 15, 80, 90, "Squats");
-
 INSERT INTO `user_preferences` (`preference_id`, `user_id`, `preferred_types`, `preferred_intensity`, `preferred_duration`, `preferred_exercise`)
 VALUES
   (1, 3601, "Strength", "Advanced", 15, "Chest Press"),
-(2, 2710, "Hypertrophy", "Intermediate", 20, "Bench Press"),
-(3, 2624, "Enndurance", "Beginner", 10, "Squats"),
-(4, 7572, "Strength", "Advanced", 30, "Deadlift"),
-(5, 9014, "Endurance", "Beginner", 25, "Squats");
-
-
-
-ERT INTO workout_plans (plan_id, user_id, plan_name, start_date, end_date, active)
-VALUES (1, 3601, 'Strength Training Plan', '2024-10-01', '2024-12-31', TRUE);
-       
-
+  (2, 2710, "Hypertrophy", "Intermediate", 20, "Bench Press"),
+  (3, 2624, "Enndurance", "Beginner", 10, "Squats"),
+  (4, 7572, "Strength", "Advanced", 30, "Deadlift"),
+  (5, 9014, "Endurance", "Beginner", 25, "Squats");
+INSERT INTO workout_plans (plan_id, user_id, plan_name, start_date, end_date, active)
+VALUES
+  (1, 3601, 'Strength Training Plan', '2024-10-01', '2024-12-31', TRUE);
 INSERT INTO workouts (workout_id, plan_id, wk_date, cals, type, intensity, duration)
 VALUES 
-(1, 1, '2024-10-01', 300, 'Strength', 'Advanced', 45),
+  (1, 1, '2024-10-01', 300, 'Strength', 'Advanced', 45),
 /*
     (2, 2, '2024-10-08', 400, 'Strength', 'Advanced', 60),
 (3, 3, '2024-10-01', 500, 'Endurance', 'Beginner', 120),
@@ -163,15 +143,15 @@ VALUES
 */
 INSERT INTO exercises (exercise_id, workout_id, api_id, plan_sets, plan_reps, plan_weight, rest_time, exercise_name)
 VALUES 
-(8, 1, 101, 4, 12, 50.0, 60, 'Bench Press'),
-(9, 1, 102, 3, 10, 60.0, 90, 'Deadlift');
-
+  (8, 1, 101, 4, 12, 50.0, 60, 'Bench Press'),
+  (9, 1, 102, 3, 10, 60.0, 90, 'Deadlift');
 INSERT INTO user_preferences (preference_id, user_id, preferred_types, preferred_intensity, preferred_duration, preferred_exercise)
-VALUES (1, 3601, 'Strength', 'Advanced', NULL, 'Bench Press');
-
+VALUES
+  (1, 3601, 'Strength', 'Advanced', NULL, 'Bench Press');
 INSERT INTO user_feedback (user_id, workout_id, rating, calories_burned)
-VALUES (3601, 1, 5, 100),
-    (2710, 94137, 6, 150),
-    (2624, 65927, 7, 200),
-    (9014, 39681, 8, 250),
-    (7572, 48867, 9, 300);
+VALUES
+  (3601, 1, 5, 100),
+  (2710, 94137, 6, 150),
+  (2624, 65927, 7, 200),
+  (9014, 39681, 8, 250),
+  (7572, 48867, 9, 300);
